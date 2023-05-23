661bd870f8b9d1560ab9e6abc1b2266ccfa8ab86
Nicholas Nikas <nnikas@microsoft.com>
2023-05-23 15:38:42 -0400

    Set up frontend

---
+++ src/components/MessagesDisplay.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+// @ts-ignore
+import MessageDisplay from "./MessageDisplay.tsx";
+
+const MessagesDisplay = () => {
+  return (
+    <div className="messages-display">
+      <MessageDisplay />
+      <MessageDisplay />
+      <MessageDisplay />
+      <MessageDisplay />
+      <MessageDisplay />
+      <MessageDisplay />
+    </div>
+  );
+};
+
+export default MessagesDisplay;
